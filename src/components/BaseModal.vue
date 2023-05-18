bfb8f8483187029f656e2fe516956c9ca763c6eb
ibepo <kefanbufan@163.com>
2023-05-18 16:14:57 +0800

    模态窗，动画，发送到根节点

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,12 +1,59 @@
 <template>
-  <div class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8 ">
-    <div class="p-4 bg-white self-start mt-32 max-w-screen-md ">
-      <solt></solt>
-      <button></button>
-    </div>
-  </div>
+  <Teleport to="body">
+    <Transition name="fade">
+      <div v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8 ">
+        <Transition name="tada">
+          <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-xl ">
+            <slot />
+            <button class="text-white mt-8 bg-weather-primary py-2 px-6" @click="$emit('close-modal')">Close</button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 
 <script setup>
 
+const props = defineProps(
+  {
+    modalActive: {
+      type: Boolean,
+      default: false,
+    },
+  }
+)
+
+const emits = defineEmits(["close-modal"])
+
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.tada-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.tada-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.tada-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.tada-leave-to {
+  transform: scale(0.8);
+}
+</style>
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="sticky top-0 bg-weather-primary shadow-lg">
-    <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+    <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6 ">
       <RouterLink :to="{ name: 'home' }">
         <div class="flex items-center gap-3 ">
           <i class="fa-solid fa-sun text-2xl"></i>
@@ -9,15 +9,31 @@
         </div>
       </RouterLink>
       <div class="flex gap-3 flex-1 justify-end">
-        <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+        <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+          @click="toggleModal"></i>
         <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
       </div>
+      <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+        <div class="text-black">
+          <h1 class="text-2xl">About</h1>
+          <h2 class="text-xl font-light ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur veritatis
+            blanditiis
+            atque eius reiciendis est natus, debitis itaque assumenda sapiente modi, porro vero voluptate id dignissimos,
+            exercitationem numquam optio facilis?</h2>
+        </div>
+      </BaseModal>
     </nav>
   </header>
 </template>
 
 <script setup>
+import { ref } from "vue"
 import { RouterLink } from 'vue-router';
+import BaseModal from './BaseModal.vue';
 
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value
+}
 
 </script>
