de38d1a3dca0ac628989b8fbcc22db2ecb214022
ibepo <kefanbufan@163.com>
2023-05-23 12:08:02 +0800

    chore:add vue-router

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col flex-1 items-center">
+    <div v-if="route.query.preview" class="text-white p-4 bg-weather-secondary w-full text-center">
+      You are currently previewing this city,click the "+" icon to start tracking this city.
+    </div>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const getWeatherData = async () => {}
+</script>
